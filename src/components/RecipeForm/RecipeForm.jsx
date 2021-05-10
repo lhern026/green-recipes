4fce9c2dc0efc32dc8594dd8c83b9d154dc03bbf
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-05-10 11:50:08 -0700

    handle change and handle submit

--- src/components/RecipeForm/RecipeForm.jsx
+++ src/components/RecipeForm/RecipeForm.jsx
@@ -1,34 +1,35 @@
 import { useEffect, useState } from "react"
 
 
-export default function RecipeForm(){
-    const [recipes,setRecipes] = useState({
+export default function RecipeForm({handleAddRecipe}){
+    const [input,setInput] = useState({
         recipe:'',
         ingredients: '',
         instructions:'',
         duration:''
     })
     function handleChange(evt){
-        setRecipes({ ...recipes, [evt.target.name]: evt.target.value });
+        setInput({ ...input, [evt.target.name]: evt.target.value });
 
     }
     async function handleSubmit(evt) {
         // Prevent form from being submitted to the server
         evt.preventDefault();
+        handleAddInput(input);
         
       }
     return(
         <>
-        <div onSubmit={handleSubmit}>
-            <form autoComplete="off" >
+        <div >
+            <form autoComplete="off"  onSubmit={handleSubmit}>
                 <label>Recipe</label>
-                <input type="text" name="recipe" value={recipes.recipe}  onChange={handleChange} required />
+                <input type="text" name="recipe" value={input.recipe}  onChange={handleChange} required />
                 <label>Ingredients</label>
-                <input type="text" name="ingredients" value={recipes.ingredients} onChange={handleChange}  required />
+                <input type="text" name="ingredients" value={input.ingredients} onChange={handleChange}  required />
                 <label>Instructions</label>
-                <input type="text" name="instructions"  value={recipes.instructions} onChange={handleChange}  required />
+                <input type="text" name="instructions"  value={input.instructions} onChange={handleChange}  required />
                 <label>Duration in minutes</label>
-                <input type="number" name="duration" value={recipes.duration} onChange={handleChange}  required />
+                <input type="number" name="duration" value={input.duration} onChange={handleChange}  required />
                 
                 <button type="submit">Submit</button>
         </form>
--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import * as userService from '../../utilities/users-service';
 
 export default function RecipePage() {
+  const [recipes,setRecipes]=useState([]);
   
   return (
     <>
