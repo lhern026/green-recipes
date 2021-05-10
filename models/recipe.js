2cef1f618a62dc4df5c117dac3955459fc0e0412
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-05-09 22:48:50 -0700

    create recipe model

---
+++ models/recipe.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const recipeSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    name: { type: String, required: true },
+    ingredients: { type: String, required: true },
+    instructions: { type: String, required: true },
+    
+    
+  }, {
+    timestamps: true
+  });
+
+
+
+module.exports = mongoose.model('Recipe', recipeSchema);
