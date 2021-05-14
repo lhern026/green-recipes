e72191a6a28c87442fd10a15896db4409e42c613
luis hernandez <luishernandez@luiss-MacBook-Air.local>
2021-05-13 23:26:58 -0700

    implement css

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -8,26 +8,26 @@ export default function LandingPage({ setUser }) {
 
   return (
     <>
-    <Anime>
+    
     <main className="landing">
       <div className="cont1">
-      <Anime translateX={[-350]} duration={[7000]} translateY={[100]} >
+      <Anime translateX={[-250]} duration={[7000]} translateY={[50]} >
       <h1>Green recipes</h1>
       </Anime>  
       <Anime translateX={[350]} duration={[2000]}  translateY={[-100]} rotate={['6turn']}>
-      <p>Share your edible recipes!</p>
+      <p className="landingP">Share your edible recipes!</p>
       </Anime> 
       </div>
       <br />
       <Anime translateX={[-150]} duration={[1000]} scale={[0.8]}>
-      <p>For those who prefer consuming their medicine in the form of an edible arrangement❤️</p></Anime>
+      <p className="forThose">For those who prefer consuming their medicine in the form of an edible arrangement❤️</p></Anime>
       
-      <Anime translateY={[100]} delay={[3000]}  duration={[1000]} scale={[1.1]}>
+      <Anime translateY={[100]} delay={[4000]}  duration={[1000]} scale={[1.1]}>
       <p>Please sign up or login returning user💙</p>
       <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'Sign Up': 'Log In'}</button>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}</Anime>
     </main>
-    </Anime>
+    
     </>
   );
 }
